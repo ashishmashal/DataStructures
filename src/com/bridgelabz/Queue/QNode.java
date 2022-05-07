0ffa1c91a0c272d4420fbfb709a23021ac6471c9
ashishmashal <ashishmashal5@gmail.com>
2022-05-07 15:18:57 +0530

    Added:EnQueue Method Added Into Queue

---
+++ src/com/bridgelabz/Queue/QNode.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.Queue;
+
+public class QNode {
+    int key;
+    QNode next;
+
+    // constructor to create a new linked list node
+    public QNode(int key)
+    {
+        this.key = key;
+        this.next = null;
+    }
+}
